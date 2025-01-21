cd59d1afb6c65666e797d21c0570fb9ed0244c4a
DKBishop1 <balex.private1@gmail.com>
2025-01-21 03:51:05 -0500

    Add files via upload

---
+++ JavaWorkSpace2024/Debug/src/Debug.java
@@ -0,0 +1,11 @@
+
+public class Debug {
+
+	public static void main(String[] args) {
+		int number1= 22;
+		int number2= 44;
+		int sum = number1 + number2;
+		System.out.println("The sum is "+ sum);
+	}
+
+}
